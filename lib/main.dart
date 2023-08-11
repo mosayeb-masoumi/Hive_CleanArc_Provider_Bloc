47342006d467f9c09911afa89928666be040bc2e
mosayeb-masoumi <mb.masoumi1000@gmail.com>
2023-08-12 00:13:16 +0430

    hive bloc clean arc

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      // home: const NoteHiveCleanArcProvider(),
-      home: const ContactScreen(),
-      // home: const TestClasss(),
+      home: const NoteHiveCleanArcProvider(),
+      // home: const ContactScreen(),
     );
   }
 }
